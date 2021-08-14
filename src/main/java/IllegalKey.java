045102a16453f588f29fb3740300089ad040cf56
getty <gcherono15@gmail.com>
2021-08-14 20:53:57 +0300

    added new methods in IllegalKey

--- src/main/java/IllegalKey.java
+++ src/main/java/IllegalKey.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+package com.company;
 
-public class IllegalKey {
+public class IllegalKey extends Exception
+{
+    int illegal_key;
+    public IllegalKey(int illegal_key)
+    {
+        this.illegal_key= illegal_key;
+    }
+
+    public String toString()
+    {
+        return
+                "Warning: Illegal key (" +illegal_key +")!!!"
+                        +"\nConsinder key from range (1 - 25)";
+    }
 }

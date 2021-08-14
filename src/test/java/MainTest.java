7d3355049e9cf9c977115aa96c2256b353ab260e
getty <gcherono15@gmail.com>
2021-08-14 22:12:07 +0300

    added a test to test for decryption of data

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,9 +10,19 @@ public class MainTest {
         Encrypt encrypt = new Encrypt();
         encrypt.setEncryption_key(5);
         encrypt.setEncrypt_this("ABC");
-        System.out.println("Original Content: " + encrypt.getEncrypt_this());
-        System.out.println("Encrypted Content: " +encrypt.encrypt_data());
+        //System.out.println("Original Content: " + encrypt.getEncrypt_this());
+        //System.out.println("Encrypted Content: " +encrypt.encrypt_data());
         assertEquals("FGH",encrypt.encrypt_data());
     }
 
+    @Test
+    public void decrypt_data_decryptFGHtoABC()
+    {
+
+        Decrypt decrypt = new Decrypt();
+        decrypt.setDecryption_key(5);6
+        decrypt.setDecrypt_this("FGH");
+        //System.out.println("Decrypted Content: " +decrypt.decrypt_data());
+        assertEquals("ABC",decrypt.decrypt_data());
+    }
 }
